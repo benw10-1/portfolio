d9eec2c00122a637e0f26d40d80070a4b6d36bb0
Ben Wirth <benjaminwirth10@gmail.com>
2022-05-07 23:21:58 -0700

    bundled

--- src/main.js
+++ src/main.js
@@ -2,25 +2,16 @@ require("@babel/polyfill");
 let { ismob, loadEls, loadMobile, loadImages, imgS } = require("./base")
 
 window.addEventListener("load", function () {
-    let d = this.document.querySelector(".intro")
-    console.log(d)
-    try {
-        if (!ismob()) {
-            if ("scrollBehavior" in document.documentElement.style) {
-                loadEls()
-                window.requestAnimationFrame(_ => {
-                    window.scroll(window.innerWidth, 0)
-                })
-            }
-            else loadMobile()
+    if (!ismob()) {
+        if ("scrollBehavior" in document.documentElement.style) {
+            loadEls()
+            window.requestAnimationFrame(_ => {
+                window.scroll(window.innerWidth, 0)
+            })
         }
         else loadMobile()
-        throw "No error"
     }
-    catch (err) {
-        console.log(err)
-        d.innerHTML = String(err)
-    }
+    else loadMobile()
 })
 
 loadImages(imgS)
